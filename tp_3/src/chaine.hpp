a28f8a16dd9ffc12bdf097b2f33d89936d24891c
andre-tavas <andretaguerra@gmail.com>
2024-11-01 18:09:45 +0100

    TP3 tests 4-5

--- tp_3/src/chaine.hpp
+++ tp_3/src/chaine.hpp
@@ -6,30 +6,44 @@
 #include "exception.hpp"
 
 template <typename T>
-std::string chaine(const T obj){
+std::string chaine(const T & obj){
     throw ExceptionChaine(obj);
 };
 
+
+
 template <typename T, typename ... OtherArgs>
-std::string chaine(T a, OtherArgs... rest){
+std::string chaine(const T& a, const OtherArgs... rest){
     chaine(a) + " " + chaine(rest...);
     return chaine(a) + " " + chaine(rest...);; 
 };
 
-template <>
-std::string chaine<std::string>(std::string obj){
-    return obj;
+template <typename T, size_t... Is>
+std::string chaine_bis(const T& t, std::index_sequence<Is...>){
+    return chaine(std::get<Is>(t)...);
+};
+
+template <typename... ARGS>
+std::string chaine(const std::tuple<ARGS...>& t){
+    return chaine_bis(t,std::make_index_sequence<sizeof...(ARGS)>());
 };
 
 template <>
-std::string chaine<int>(const int obj){
+std::string chaine<std::string>(const std::string& obj){
     std::stringstream ss;
     ss << obj;
     return ss.str();
 };
 
 template <>
-std::string chaine<double>(double obj){
+std::string chaine<int>(const int& obj){
+    std::stringstream ss;
+    ss << obj;
+    return ss.str();
+};
+
+template <>
+std::string chaine<double>(const double& obj){
     return std::to_string(obj);
 };
 
